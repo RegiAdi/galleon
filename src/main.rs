6bdbca1362ddd04abc1e45394a5e4864c637669e
Regi Adi Priyanto <regiadi.priyanto@gmail.com>
2023-09-04 21:33:03 +0700

    Add MongoDB Rust driver and connection

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,35 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+use mongodb::{
+	bson::doc, 
+	options::{ClientOptions, ServerApi, ServerApiVersion}, 
+	Client
+};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> mongodb::error::Result<()> {
 	// initialize tracing
 	tracing_subscriber::fmt::init();
+
+	// replace the placeholder with your Atlas connection string
+	let uri = "mongodb://localhost:27017";
+	let mut client_options = ClientOptions::parse(uri).await?;
+
+	// set the server_api field of the client_options object to Stable API version 1
+	let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+	client_options.server_api = Some(server_api);
+
+	// create a new client and connect to the server
+	let client = Client::with_options(client_options)?;
+
+	// send a ping to confirm a successful connection
+	client
+		.database("pos_mobile")
+		.run_command(doc! {"ping": 1}, None)
+		.await?;
+
+	println!("Pinged your deployment. You successfully connected to MongoDB!");
 
 	// build our application with a route
 	let app = Router::new()
@@ -18,23 +42,32 @@ async fn main() {
 
 	// run our app with hyper, listening globally on port 3000
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
 	tracing::debug!("listening on {}", addr);
+	println!("listening on {}", addr);
+
 	axum::Server::bind(&addr)
 		.serve(app.into_make_service())
 		.await
 		.unwrap();
+
+	Ok(())
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
+	println!("root()");
+
 	"Hello, World!"
 }
 
 async fn create_user(
 	// this argument tells axum to parse the request body
 	// as JSON into a `CreateUser` type
-	Json(payload): Json<CreateUser>,
+	Json(payload): Json<CreateUserReq>,
 ) -> (StatusCode, Json<User>) {
+	println!("create_user()");
+
 	// insert your application logic here
 	let user = User {
 		id: 1337,
@@ -48,7 +81,7 @@ async fn create_user(
 
 // the input to our `create_user` handler
 #[derive(Deserialize)]
-struct CreateUser {
+struct CreateUserReq {
 	username: String,
 }
 
